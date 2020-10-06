7fcebdaf7998266ae7b1515179b1483993e88448
Johnny Chiu <johnny@blockchain.news>
2020-10-06 10:52:10 +0800

    cm

--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -89,9 +89,9 @@ export function tryParseAmount(value?: string, currency?: Currency): CurrencyAmo
 }
 
 const BAD_RECIPIENT_ADDRESSES: string[] = [
-  '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f', // v2 factory
+  '0x83880858faefC856bF410Bb90F1CFBC03D9681aE', // v2 factory
   '0xf164fC0Ec4E93095b804a4795bBe1e041497b92a', // v2 router 01
-  '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D' // v2 router 02
+  '0xb1351AdF8FC570a1De7bE7BA4896FbC8d6900310' // v2 router 02
 ]
 
 /**
